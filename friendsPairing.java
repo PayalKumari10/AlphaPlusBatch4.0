a3883e342b2766e4299db66aae903149734a63b4
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-12-06 23:25:27 +0530

    Create friendsPairing.java

---
+++ friendsPairing.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static int friendsPairing(int n) {
+        if(n == 1 || n == 2) {
+            return n;
+        }
+        return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    }
+    public static void main(String args[]) {
+        System.out.println(friendsPairing (3));
+    }
+}
+
+
+
+=== Code Output ===
+4
+
+=== Code Execution Successful ===
