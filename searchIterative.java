4b03abeaf3ddfd4235dcb0e8133a8685b959c7b9
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-12-24 16:03:54 +0530

    Create searchIterative.java

---
+++ searchIterative.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    // Add a node at the beginning
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add a node at the end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Add a node at a specific index
+    public void add(int index, int data) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null && i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        if (temp == null) {
+            System.out.println("Index out of bounds");
+            return;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Print the linked list
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Search iteratively for a key in the linked list
+    public int itrSearch(int key) {
+        Node temp = head;
+        int index = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2, 3);
+
+        ll.print();
+
+        System.out.println(ll.itrSearch(3));  // Output: 2
+        System.out.println(ll.itrSearch(10)); // Output: -1
+    }
+}
+
+
+
+
+=== Code Output ===
+
+1 -> 2 -> 3 -> 4 -> 5 -> null
+2
+-1
+
+=== Code Execution Successful ===
