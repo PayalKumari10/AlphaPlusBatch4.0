ec898ae222a73e0c03d9289ce1e88c335a9ea236
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2025-01-14 17:20:32 +0530

    Update transformtoSumTreeBS.java

--- transformtoSumTreeBS.java
+++ transformtoSumTreeBS.java
@@ -21,26 +21,6 @@ public class Main {
         }
     }
    
-    public static int KAncestor(Node root, int n, int k) {
-        if(root == null) {
-            return -1;
-        }
-        if(root.data == n) {
-            return 0;
-        }
-        int leftDist = KAncestor(root.left, n, k);
-        int rightDist = KAncestor(root.right, n, k);
-        
-        if(leftDist == -1 && rightDist == -1) {
-            return -1;
-        }
-        int max = Math.max(leftDist, rightDist);
-        if(max + 1 == k) {
-            System.out.println(root.data);
-        }
-        return max + 1;
-    }
-
     public static int transform(Node root) {
         if(root == null) {
             return 0;
