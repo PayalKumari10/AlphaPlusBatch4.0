c5515b7be221bb278156b828c04c5a2f67b07218
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2025-01-14 16:26:19 +0530

    Update kthAncestorBS.java

--- kthAncestorBS.java
+++ kthAncestorBS.java
@@ -21,69 +21,6 @@ public class Main {
         }
     }
    
-    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
-        if(root == null) {
-            return false;
-        }
-        path.add(root);
-        
-        if(root.data == n) {
-            return true;
-        }
-        boolean foundLeft = getPath(root.left, n, path);
-        boolean foundRight = getPath(root.right, n, path);
-        
-        if(foundLeft || foundRight) {
-            return true;
-        }
-        
-        path.remove(path.size()-1);
-           return false;
-    }
-    
-    
-    public static Node lca2(Node root, int n1, int n2) {
-        if(root == null || root.data == n1 || root.data == n2) {
-            return root;
-        }
-        Node leftLca = lca2(root.left, n1, n2);
-        Node rightLca = lca2(root.right, n1, n2);
-        
-        if(rightLca == null) {
-            return leftLca;
-        }
-        if(leftLca == null) {
-            return rightLca;
-        }
-        return root;
-    }
-    
-    public static int lcaDist(Node root, int n) {
-        if(root == null) {
-            return -1;
-        }
-        if(root.data == n) {
-            return 0;
-        }
-        int leftDist = lcaDist(root.left, n);
-        int rightDist = lcaDist(root.right, n);
-        
-        if(leftDist == -1 && rightDist == -1) {
-            return -1;
-        } else if(leftDist == -1) {
-            return rightDist+1;
-        } else {
-            return leftDist+1;
-        }
-    }
-    public static int minDist(Node root, int n1, int n2) {
-        Node lca = lca2(root, n1, n2);
-        int dist1 = lcaDist(lca, n1);
-        int dist2 = lcaDist(lca, n2);
-        
-        return dist1 + dist2;
-    }
-    
     public static int KAncestor(Node root, int n, int k) {
         if(root == null) {
             return -1;
@@ -117,9 +54,3 @@ public class Main {
         KAncestor(root, n, k);
     }
 }
-
-=== Code output ===
-
-1
-
-=== Code Execution Successful ===
