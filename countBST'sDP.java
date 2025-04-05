e503f03c635e74caf2de40bea525e31e0b8a9c12
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2025-04-05 22:23:55 +0530

    Update countBST'sDP.java

--- countBST'sDP.java
+++ countBST'sDP.java
@@ -8,7 +8,7 @@ public class Classroom {
         dp[1] = 1;
         
         for(int i=2; i<n+1; i++) {
-            //
+            //Ci -> BST (i nodes) -> dp[i]
             for(int j=0; j<i; j++) {
                 int left = dp[j];
                 int right = dp[i-j-1];
