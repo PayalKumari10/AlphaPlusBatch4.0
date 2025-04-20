3820784030f220271de2ea3267dbdcd7dcf8bb5b
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2025-04-21 00:48:46 +0530

    Create MaxorMin_Element_Queries..java

---
+++ MaxorMin_Element_Queries..java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Classroom {
+    static int tree[];
+    
+    public static void init(int n) {
+        tree = new int[4*n];
+    }
+    
+    public static void buildTree(int i, int si, int sj, int arr[]) {
+        if(si == sj) {
+            tree[i] = arr[si];
+            return;
+        }
+        int mid = (si + sj)/2; 
+        buildTree(2*i+1, si, mid, arr);
+        buildTree(2*i+2, mid+1, sj, arr);
+        
+        tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
+        }
+    
+    public static void main(String args[]) {
+        int arr[] = {6, 8, -1, 2, 17, 1, 3, 2, 4};
+        int n = arr.length;
+        init(n);
+        buildTree(0, 0, n-1,arr);
+        
+        for(int i=0; i<tree.length; i++) {
+            System.out.print(tree[i]+" ");
+        }
+    }
+}
+
+
+
+=== Code Output ===
+17 17 4 8 17 3 4 8 -1 2 17 1 3 2 4 6 8 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+=== Code Execution Successful ===
